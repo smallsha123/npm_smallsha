bb858c5e5c37bda82e7b6ccf046626556f91a3b7
smallshaxiaoche <15239851762@163.com>
2019-02-13 16:11:23 +0800

    ls

--- common/js/mobile_fixable.js
+++ common/js/mobile_fixable.js
@@ -7,7 +7,7 @@
             if(clientWidth>=640){
                 docEl.style.fontSize = '100px';
             }else{
-                docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+                docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
             }
         };
     if (!doc.addEventListener) return;
